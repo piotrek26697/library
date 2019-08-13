b6d06d13ccb685cc8f94ce26124a2f2f9cda0f5e
piotr.musiol <piotrek26697@gmail.com>
2019-08-13 16:17:35 +0200

    Update BookHireEndpoint

--- src/main/java/pl/fis/endpoints/BookHireEndpoint.java
+++ src/main/java/pl/fis/endpoints/BookHireEndpoint.java
@@ -5,6 +5,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -12,6 +13,7 @@ import javax.ws.rs.core.Response.Status;
 
 import pl.fis.daos.BasicDAO;
 import pl.fis.data.entities.BookHire;
+import pl.fis.errors.ResourceNotFoundException;
 
 @Path("/book_hires")
 public class BookHireEndpoint
@@ -33,5 +35,16 @@ public class BookHireEndpoint
 	{
 		hireManager.addObject(bookHire);
 		return Response.status(Status.NO_CONTENT).build();
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{id}")
+	public Response getBookHire(@PathParam("id") long id)
+	{
+		BookHire bookHire = hireManager.getObject(BookHire.class, id);
+		if(bookHire == null)
+			throw new ResourceNotFoundException();
+		return Response.ok(bookHire).build();
 	}
 }
