173df0ffecf75b341ca52ea71f46ca8126a37b68
piotr.musiol <piotrek26697@gmail.com>
2019-08-13 13:39:44 +0200

    Implement generic DAO for all entities

---
+++ src/main/java/pl/fis/daos/BasicDAO.java
@@ -0,0 +1,31 @@
+package pl.fis.daos;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+public class BasicDAO<T>
+{
+	@PersistenceContext
+	private EntityManager em;
+
+	public void addObject(T obj)
+	{
+		em.persist(obj);
+	}
+
+	public List<T> getObjects(String name)
+	{
+		Query query = em.createQuery("select c from " + name + " c");
+		@SuppressWarnings("unchecked")
+		List<T> objList = query.getResultList();
+		return objList;
+	}
+
+	public T getObject(Class<T> clazz, long id)
+	{
+		return em.find(clazz, id);
+	}
+}
